beb9eb6b62061e3ad36b27a831a260ffa8f4bca6
Allan Yin <allanyin@AllanMacBookPro.local>
2023-05-06 12:50:51 -0400

    project initialization

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
